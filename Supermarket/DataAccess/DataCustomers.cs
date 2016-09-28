c86f45bc7daf2d4015d7a7e43947699de7ab620b
delgadoguadamuz <delgadoguadamuz@hotmail.com>
2016-09-27 18:47:05 -0600

    commit

---
+++ Supermarket/DataAccess/DataCustomers.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Context;
+
+namespace DataAccess
+{
+   public class DataCustomers
+    {
+
+        public List<Customer> SelectAll()
+        {
+            using (supermarket mySupermarket = new supermarket())
+            {
+                List<Customer> allCustomers = mySupermarket.Customers.ToList();
+
+                return allCustomers;
+
+            }
+
+        }
+        public Customer SelectById(int id)
+        {
+
+            using (supermarket mySupermarket = new supermarket())
+            {
+
+                Customer customerFromId = mySupermarket.Customers
+
+                .Where(Customer => Customer.CustomerId == id)
+                .SingleOrDefault();
+
+                return customerFromId;
+            }
+        }
+
+        public List<Customer> SelectByFrom (String Address)
+        {
+
+            using (supermarket mySupermarket = new supermarket())
+            {
+               List<Customer> customerList = mySupermarket.Customers
+
+               .Where(Customer => Customer.Address.Contains(Address))
+               .ToList();
+
+                return customerList;
+
+            }
+         }
+
+        public void Insert(Customer pCustomer)
+        {
+
+
+            using (supermarket mySupermarket = new supermarket())
+            {
+                mySupermarket.Customers.Add(pCustomer);
+                mySupermarket.SaveChanges();
+
+            }
+        }
+        
+        public void Update (Customer pCustomer)
+        {
+
+
+            using (supermarket mySupermarket = new supermarket())
+            {
+
+                Customer customerObj = mySupermarket.Customers
+
+                .Where(Customer => Customer.CustomerId == pCustomer.CustomerId)
+                .SingleOrDefault();
+
+                if (customerObj != null)
+                {
+                    customerObj.Address = pCustomer.Address;
+                    customerObj.FistName = pCustomer.FistName;
+                    customerObj.LastName = pCustomer.LastName;
+                    customerObj.Telephone = pCustomer.Telephone;
+
+                    mySupermarket.SaveChanges();
+                }
+            }
+
+
+            }
+
+        }
+     }
+
+
+ 
+
